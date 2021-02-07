4901061414ceb0396bede18935cbf9f4ffa7efe1
heejjuunn <heejjuunn@gmail.com>
2021-02-07 14:52:17 +0900

    api connect

--- app/src/main/java/com/hjhj/daedan/Tab1_MapFragment.java
+++ app/src/main/java/com/hjhj/daedan/Tab1_MapFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,12 +28,31 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import static androidx.core.content.ContextCompat.checkSelfPermission;
 
 public class Tab1_MapFragment extends Fragment {
     GoogleMap gMap;
     MapView mapView;
     MarkerOptions myMarker = null;
+    TextView tv_temperature;
+    String weather, temperature;
+    String date, time, currentTime, currentTime1, currentTime2;
+    String server;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,6 +70,13 @@ public class Tab1_MapFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.page_map_tab1, container, false);
+
+        tv_temperature = view.findViewById(R.id.page_map_tv_temperature);
+
+        getTime();
+        getInfo();
+
+
         mapView = (MapView) view.findViewById(R.id.mappage_mapview);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
@@ -83,6 +110,131 @@ public class Tab1_MapFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void getInfo() {
+        String api = "ZkmHEAVaoQ0yFwBbRFQSMJkOhXX%2FMQzcTpYDB0Q513dcVb3Vuz6vCU7QSEPdyYs0A3aOUSDG2WuzVo%2BQDF4beQ%3D%3D";
+
+        String url1 = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=";
+        String url2 = "&numOfRows=100&pageNo=1&dataType=XML&base_date=";
+        server = url1+api+url2+date+"&base_time="+time+"&nx="+"55"+"&ny="+"127";
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    Log.e("Test","work0");
+                    URL url = new URL(server);
+                    Log.e("Test","work1");
+                    InputStream is = url.openStream();
+                    Log.e("Test","work2");
+                    InputStreamReader isr = new InputStreamReader(is);
+                    Log.e("Test","work3");
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    XmlPullParser xpp = factory.newPullParser();
+                    xpp.setInput(isr);
+
+                    int eventType = xpp.getEventType();
+
+                    while(eventType!= XmlPullParser.END_DOCUMENT){
+                        switch (eventType){
+                            case  XmlPullParser.START_DOCUMENT:
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getActivity(), "start!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                break;
+                            case XmlPullParser.START_TAG:
+                                String tagName = xpp.getName();
+                                if(tagName.equals("category")){
+                                    xpp.next();
+                                    if(xpp.getText().equals("T1H")){
+                                        for(int i=0;i<9;i++){
+                                            xpp.next();
+                                        }
+                                        temperature = xpp.getText();
+                                    }
+                                    //- 강수형태(PTY) 코드 : 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4), 빗방울(5), 빗방울/눈날림(6), 눈날림(7)
+                                    if(xpp.getText().equals("PTY")){
+                                        for(int i=0;i<9;i++){
+                                            xpp.next();
+                                        }
+                                        weather = xpp.getText();
+
+                                    }
+                                }
+                                break;
+
+                            case XmlPullParser.TEXT:
+                                break;
+                            case XmlPullParser.END_TAG:
+                                break;
+
+                        }
+                        eventType = xpp.next();
+                    }
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv_temperature.setText(temperature+"&"+weather);
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
+    private void getTime() {
+        TimeZone zone = TimeZone.getTimeZone("Asia/Seoul");  // TimeZone에 표준시 설정
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.KOREAN);
+        dateFormat.setTimeZone(zone);                    //DateFormat에 TimeZone 설정
+
+        DateFormat timeFormat = new SimpleDateFormat("HHmm", Locale.KOREAN);
+        timeFormat.setTimeZone(zone);
+
+        date = dateFormat.format(new Date());
+        String currentTime = timeFormat.format(new Date());
+        //t시 40분이 넘으면 t시, 안넘으면 t-1시..단 t-1이 -1이 되면 23시로.
+        String currentTime1 = currentTime.substring(0,2);
+        String currentTime2 = currentTime.substring(2);
+
+        if(Integer.parseInt(currentTime1)<11){
+            if(Integer.parseInt(currentTime2) >40){
+                time = currentTime1+"00";
+            }else{
+                if(Integer.parseInt(currentTime1)-1==-1){
+                    currentTime1 = 23+"";
+                }else{
+                    currentTime1 = "0"+(Integer.parseInt(currentTime1)-1);
+                }
+                time = currentTime1+"00";
+            }
+        }else{
+            if(Integer.parseInt(currentTime2) >40){
+                time = currentTime1+"00";
+            }else{
+                if(Integer.parseInt(currentTime1)-1==-1){
+                    currentTime1 = 23+"";
+                }else{
+                    currentTime1 = ""+(Integer.parseInt(currentTime1)-1);
+                }
+                time = currentTime1+"00";
+            }
+        }
+
     }
 
     @Override
